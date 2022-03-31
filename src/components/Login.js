ff901864c2e82bef3939a6f03d917feb2544be2d
Tsankov03 <95347481+Tsankov03@users.noreply.github.com>
2022-03-31 13:52:24 +0300

    Minor fixes. Broken Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import { GoogleOutlined,FacebookOutlined, MailOutlined } from "@ant-design/icons";
 import firebase from "firebase/app";
 import { auth } from "../firebase";
 
@@ -16,14 +16,22 @@ const Login = () => {
         >
           <GoogleOutlined /> Sign in With Google
         </div>
-{/*         <div
+        <div
           className="login-button facebook"
           onClick={() =>
             auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
           }
         >
           <FacebookOutlined /> Sign in With Facebook
-        </div> */}
+        </div>
+        <div
+          className="login-button email"
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.EmailAuthProvider())
+          }
+        >
+          <MailOutlined /> Sign in with Email
+        </div>
       </div>
     </div>
   );
